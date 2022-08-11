eeb3a7c569927eae274467ccd14d31720cf65b5b
seohyun319 <tjguselly0319@gmail.com>
2022-08-11 17:55:50 +0900

    Chore: set mine page

---
+++ pages/applications/mine.tsx
@@ -0,0 +1,8 @@
+import type { NextPage } from 'next';
+import styled from '@emotion/styled/macro';
+import tw from 'twin.macro';
+
+const TreasureMine: NextPage = () => {
+  return <div>TreasureMine페이지!</div>;
+};
+export default TreasureMine;
