01bca9dd34f0723792214bf827665965e8e9b51a
Bong <qhdgkdbs@gmail.com>
2022-08-11 23:30:45 +0900

    Feat: Add ios status bar

--- components/common/Header.tsx
+++ components/common/Header.tsx
@@ -1,3 +1,79 @@
-export const Header = () => {
-  return <div>Header</div>;
+import tw from 'twin.macro';
+import styled from '@emotion/styled';
+import { TbAntennaBars4, TbWifi } from 'react-icons/tb';
+import { BsBatteryFull } from 'react-icons/bs';
+
+const Header = () => {
+  return (
+    <Container>
+      <Left>WE +</Left>
+      <Notch />
+      <VirtualNotch />
+      <Right>
+        <TbAntennaBars4 size="1.5rem" />
+        <TbWifi size="1.5rem" />
+        <BsBatteryFull size="1.5rem" />
+      </Right>
+    </Container>
+  );
 };
+
+const VirtualNotch = styled.div`
+  width: 200px;
+  height: 2.5rem;
+`;
+
+const Notch = styled.div`
+  position: absolute;
+  top: 0;
+  left: 50%;
+  width: 200px;
+  height: 2.5rem;
+  background-color: black;
+  border-radius: 0 0 1.5rem 1.5rem;
+  transform: translateX(-50%);
+  &:before,
+  &:after {
+    content: '';
+    position: absolute;
+    left: -7px;
+    width: 14px;
+    height: 7px;
+    background-size: 50% 100%;
+    background-repeat: no-repeat;
+    background-image: radial-gradient(circle at 0 100%, transparent 7px, black 7px);
+  }
+  &:after {
+    left: 100%;
+    background-image: radial-gradient(circle at 100% 100%, transparent 7px, black 7px);
+  }
+`;
+
+const Container = tw.div`
+  flex
+  justify-evenly
+  top-0
+  left-0
+  right-0
+  mx-auto
+  w-full
+  min-w-[300px]
+  h-[2.5rem]
+  items-center
+  px-1
+`;
+
+const Right = tw.div`
+  text-center
+  w-[20%]
+  flex
+  flex-row
+  gap-1
+`;
+
+const Left = tw.div`
+  text-center
+  w-[20%]
+`;
+
+export default Header;
--- pages/index.tsx
+++ pages/index.tsx
@@ -1,28 +1,14 @@
 import type { NextPage } from 'next';
 import styles from '../styles/Home.module.css';
-import tw from 'twin.macro';
+import Header from '../components/common/Header';
 
 const Home: NextPage = () => {
   return (
     <div className={styles.container}>
-      <Notch />
+      <Header />
       <main className={styles.main}>{/* <div>아이폰 잠금화면</div> */}</main>
     </div>
   );
 };
 
 export default Home;
-
-const Notch = tw.div`
-  absolute
-  top-0
-  left-0
-  right-0
-  w-[50%]
-  max-w-[234px]
-  h-[2.5rem]
-  bg-black
-  rounded-b-3xl
-  transform
-  mx-auto
-`;
