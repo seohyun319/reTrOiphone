8b1d90e06013de5e3e5203df56ae248e3fa56710
seohyun319 <tjguselly0319@gmail.com>
2022-09-06 22:33:10 +0900

    Feat: set openBox reducer

--- _reducers/applications/treasureIsMineReducer.ts
+++ _reducers/applications/treasureIsMineReducer.ts
@@ -1,17 +1,27 @@
 import { createSlice } from '@reduxjs/toolkit';
 import { RootState } from '../store';
 
-export interface TreasureIsMineState {}
+export interface TreasureIsMineState {
+  isOpen: Array<Array<number>>;
+}
 
-const initialState: TreasureIsMineState = {};
+const initialState: TreasureIsMineState = {
+  isOpen: [],
+};
 
 export const TreasureIsMineSlice = createSlice({
   name: 'treasureIsMineReducer',
   initialState,
-  reducers: {},
+  reducers: {
+    openBox: (state, action) => {
+      const rowIdx = action.payload.rowIdx;
+      const boxIdx = action.payload.boxIdx;
+      state.isOpen[rowIdx][boxIdx] = 1;
+    },
+  },
 });
 
-export const {} = TreasureIsMineSlice.actions;
+export const { openBox } = TreasureIsMineSlice.actions;
 
 export const getTreasureIsMine = (state: RootState) => state.treasureIsMine;
 
