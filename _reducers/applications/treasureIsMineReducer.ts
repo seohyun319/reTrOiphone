0ebf326fb3c2dd1d4cd567bf965475a88d9fb9fd
seohyun319 <tjguselly0319@gmail.com>
2022-09-25 16:20:35 +0900

    Feat: set startGame on reducer

--- _reducers/applications/treasureIsMineReducer.ts
+++ _reducers/applications/treasureIsMineReducer.ts
@@ -2,10 +2,18 @@ import { createSlice } from '@reduxjs/toolkit';
 import { RootState } from '../store';
 
 export interface TreasureIsMineState {
+  numX: number;
+  numY: number;
+  numBomb: number;
+  bombContainer: Array<Array<number>>;
   isOpen: Array<Array<number>>;
 }
 
 const initialState: TreasureIsMineState = {
+  numX: 5,
+  numY: 5,
+  numBomb: 3,
+  bombContainer: [],
   isOpen: [],
 };
 
@@ -13,15 +21,19 @@ export const TreasureIsMineSlice = createSlice({
   name: 'treasureIsMineReducer',
   initialState,
   reducers: {
+    startGame: (state, action) => {
+      state.bombContainer = action.payload.bombContainer;
+      state.isOpen = action.payload.isOpen;
+    },
     openBox: (state, action) => {
       const rowIdx = action.payload.rowIdx;
-      const boxIdx = action.payload.boxIdx;
-      state.isOpen[rowIdx][boxIdx] = 1;
+      const colIdx = action.payload.colIdx;
+      state.isOpen[rowIdx][colIdx] = 1;
     },
   },
 });
 
-export const { openBox } = TreasureIsMineSlice.actions;
+export const { startGame, openBox } = TreasureIsMineSlice.actions;
 
 export const getTreasureIsMine = (state: RootState) => state.treasureIsMine;
 
