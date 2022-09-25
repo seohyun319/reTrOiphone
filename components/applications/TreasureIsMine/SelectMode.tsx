5f788333f78aa788fef407ca5453f2382135a5db
seohyun319 <tjguselly0319@gmail.com>
2022-09-26 03:30:19 +0900

    Feat: make SelectMode - put renderGrid func, startGame btn

---
+++ components/applications/TreasureIsMine/SelectMode.tsx
@@ -0,0 +1,32 @@
+import tw from 'twin.macro';
+import { useAppDispatch, useAppSelector } from '../../../hooks/common/useRedux';
+import { getTreasureIsMine, startGame } from '../../../_reducers/applications/treasureIsMineReducer';
+
+export const SelectMode = () => {
+  const game = useAppSelector(getTreasureIsMine);
+  const dispatch = useAppDispatch();
+
+  const BombContainer = new Array(game.numX); // 지뢰 여부 저장
+  const isOpen = new Array(game.numX); // 칸 클릭 여부 저장
+
+  const renderGrid = () => {
+    for (let i = 0; i < game.numX; i++) {
+      // 지뢰 컨테이너: 1이면 지뢰 존재, 0이면 없음
+      BombContainer[i] = Array.from({ length: game.numY }, () =>
+        // 지뢰 0 1 랜덤 값
+        Math.round(Math.random() * 1),
+      );
+      isOpen[i] = Array.from({ length: game.numY }).fill(0);
+    }
+  };
+  renderGrid();
+
+  return <StartGame onClick={() => dispatch(startGame({ bombContainer: BombContainer, isOpen: isOpen }))}>게임 시작</StartGame>;
+};
+
+const StartGame = tw.div`
+  bg-blue-300
+  p-2 w-max
+  rounded-lg
+  cursor-pointer
+`;
